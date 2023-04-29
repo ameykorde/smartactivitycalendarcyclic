8074040530c7b94dfc5b18cfbb7ac6810ac4fbb2
ameykorde <72167288+ameykorde@users.noreply.github.com>
2023-04-29 12:05:34 +0530

    changed route

--- routes/authRoute.mjs
+++ routes/authRoute.mjs
@@ -3,8 +3,8 @@ import { registerUser, loginUser,getUserData,deleteUser,updateUserStatus } from 
 import verify from '../middleware/verifyToken.mjs';
 const router = express.Router();
 // Register routes
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register/post', registerUser);
+router.post('/login/post', loginUser);
 
 // Routes for admin controller
 router.get('/getUser',verify, getUserData);
